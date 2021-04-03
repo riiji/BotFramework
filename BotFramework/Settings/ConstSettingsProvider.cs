eeeffb4bd0720b512a9d17fa8d1a81b835a30a6b
Fredi Kats <fredikats@outlook.com>
2021-04-04 00:34:25 +0300

    feat: add ConstSettingsProvider

---
+++ BotFramework/Settings/ConstSettingsProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace Tef.BotFramework.Settings
+{
+    public class ConstSettingsProvider<TSettings> : ISettingsProvider<TSettings>
+    {
+        private readonly TSettings _settings;
+
+        public ConstSettingsProvider(TSettings settings)
+        {
+            _settings = settings;
+        }
+
+        public TSettings GetSettings()
+        {
+            return _settings;
+        }
+    }
+}
--- BotFrameworkConsoleTest/Program.cs
+++ BotFrameworkConsoleTest/Program.cs
@@ -1,20 +1,21 @@
 ﻿using System.Threading.Tasks;
-using Tef.BotFramework.ApiProviders.VK;
+using Tef.BotFramework.ApiProviders.Telegram;
 using Tef.BotFramework.Core;
 using Tef.BotFramework.Core.BotCommands;
 using Tef.BotFramework.Settings;
 
 namespace BotFrameworkConsoleTest
 {
-    class Program
+    public static class Program
     {
         private static async Task MainAsync()
         {
+            var telegramToken = string.Empty;
 
-            var vkSettings = new ConfigSettingsProvider<VkSettings>("TelegramConfig.json");
-            var telegramApi = new VkFixedApiProvider(vkSettings);
+            var settings = new ConstSettingsProvider<TelegramSettings>(new TelegramSettings(telegramToken));
+            var api = new TelegramApiProvider(settings);
 
-            new Bot(telegramApi)
+            new Bot(api)
                 .AddDefaultLogger()
                 .SetPrefix('!')
                 .WithoutCaseSensitiveCommands()
@@ -23,8 +24,6 @@ namespace BotFrameworkConsoleTest
 
             await Task.Delay(-1);
         }
-
-        
 
         private static void Main() => MainAsync().GetAwaiter().GetResult();
     }
