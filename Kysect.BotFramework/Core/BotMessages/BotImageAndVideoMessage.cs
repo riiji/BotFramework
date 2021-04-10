6396c755e3dfd9d109906b0061ab84a6bdae9265
Alexandr <alex8.ivanov@gmail.com>
2021-04-10 23:43:42 +0300

    Feat: add BotImageAndVideoMessage

---
+++ Kysect.BotFramework/Core/BotMessages/BotImageAndVideoMessage.cs
@@ -0,0 +1,25 @@
+﻿using Kysect.BotFramework.ApiProviders;
+
+namespace Kysect.BotFramework.Core.BotMessages
+{
+    public class BotImageAndVideoMessage : IBotMessage
+    {
+        public string Text { get; }
+        public string ImagePath { get; }
+
+        public string VideoPath { get; }
+        
+        public BotImageAndVideoMessage(string text, string imagePath, string videoPath)
+        {
+            Text = text;
+            ImagePath = imagePath;
+            VideoPath = videoPath;
+        }
+        
+        public void Send(IBotApiProvider apiProvider, SenderInfo sender)
+        {
+            apiProvider.SendImage(ImagePath, Text, sender);
+            apiProvider.SendVideo(VideoPath, "", sender);
+        }
+    }
+}
