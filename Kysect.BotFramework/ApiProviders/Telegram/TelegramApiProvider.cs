ef308f15af558b5f73a17615a301159385645b0a
ManureBeetle <46840980+ManureBeetle@users.noreply.github.com>
2021-05-10 20:26:33 +0300

    Add method GetFileLink to TelegramApiProvider

--- Kysect.BotFramework/ApiProviders/Telegram/TelegramApiProvider.cs
+++ Kysect.BotFramework/ApiProviders/Telegram/TelegramApiProvider.cs
@@ -21,12 +21,16 @@ namespace Kysect.BotFramework.ApiProviders.Telegram
         private readonly TelegramSettings _settings;
         private TelegramBotClient _client;
 
+        private InputMedia fileToSend;
         public TelegramApiProvider(ISettingsProvider<TelegramSettings> settingsProvider)
         {
             _settings = settingsProvider.GetSettings();
             Initialize();
         }
-
+        public string GetFileLink()
+        {
+            return $"https://api.telegram.org/file/bot{_settings.AccessToken}/{_client.GetFileAsync(fileToSend.FileId).Result.FilePath}";
+        }
         public event EventHandler<BotEventArgs> OnMessage;
 
         public Result<string> SendText(string text, SenderInfo sender)
@@ -49,7 +53,7 @@ namespace Kysect.BotFramework.ApiProviders.Telegram
         public Result<string> SendMedia(IBotMediaFile mediaFile, string text, SenderInfo sender)
         {
             var stream = File.Open(mediaFile.Path, FileMode.Open);
-            var fileToSend = new InputMedia(stream, mediaFile.Path.Split(Path.DirectorySeparatorChar).Last());
+            fileToSend = new InputMedia(stream, mediaFile.Path.Split(Path.DirectorySeparatorChar).Last());
             var task = mediaFile.MediaType switch
             {
                 MediaTypeEnum.Photo => _client.SendPhotoAsync(sender.GroupId, fileToSend, text),
