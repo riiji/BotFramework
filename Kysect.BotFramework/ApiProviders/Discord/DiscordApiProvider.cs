7a2f1e3faf42e7840a4a460f98cc4afd15b53a41
Bibletoon <alex8.ivanov@gmail.com>
2021-06-16 21:14:47 +0300

    refactor: discord api provider

--- Kysect.BotFramework/ApiProviders/Discord/DiscordApiProvider.cs
+++ Kysect.BotFramework/ApiProviders/Discord/DiscordApiProvider.cs
@@ -20,110 +20,25 @@ namespace Kysect.BotFramework.ApiProviders.Discord
         private readonly object _lock = new();
         private readonly DiscordSettings _settings;
         private DiscordSocketClient _client;
+        
+        public event EventHandler<BotEventArgs> OnMessage;
 
         public DiscordApiProvider(ISettingsProvider<DiscordSettings> settingsProvider)
         {
             _settings = settingsProvider.GetSettings();
             Initialize();
         }
-
-        public event EventHandler<BotEventArgs> OnMessage;
-
-        public Result<string> SendText(string text, SenderInfo sender)
+        
+        private void Initialize()
         {
-            string message = "Error while sending message";
-            switch (text.Length)
-            {
-                case (0):
-                {
-                    LoggerHolder.Instance.Error($"The message wasn't sent by the command " +
-                                                $"\"{PingCommand.Descriptor.CommandName}\", the length must not be zero.");
-                    return Result.Fail(new Error(message).CausedBy(text));
-                }
-                case ( > 2000):
-                {
-                    string subString = text.Substring(0, 99) + "...";
-                    LoggerHolder.Instance.Error($"The message wasn't sent by the command " +
-                                                $"\"{PingCommand.Descriptor.CommandName}\", the length is too big: {subString}");
-                    return Result.Fail(new Error(message).CausedBy(subString));
-                }
-            }
-            
-            var task = _client.GetGuild((ulong) sender.GroupId)
-                .GetTextChannel((ulong) sender.UserSenderId)
-                .SendMessageAsync(text);
-            
-            try
-            {
-                task.Wait();
-                return Result.Ok("Message send");
-            }
-            catch (Exception e)
-            {
-                message = "Error while sending message";
-                LoggerHolder.Instance.Error(e, message);
-                return Result.Fail(new Error(message).CausedBy(e));
-            }
+            _client = new DiscordSocketClient();
+
+            _client.LoginAsync(TokenType.Bot, _settings.AccessToken);
+
+            _client.MessageReceived += ClientOnMessage;
+            _client.StartAsync();
         }
-
-        public Result<string> SendMedia(IBotMediaFile mediaFile, string text, SenderInfo sender)
-        {
-            var task = _client.GetGuild((ulong) sender.GroupId)
-                .GetTextChannel((ulong) sender.UserSenderId)
-                .SendFileAsync(mediaFile.Path, text);
-            try
-            {
-                task.Wait();
-                return Result.Ok("Message send");
-            }
-            catch (Exception e)
-            {
-                const string message = "Error while sending message";
-                LoggerHolder.Instance.Error(e, message);
-                return Result.Fail(new Error(message).CausedBy(e));
-            }
-        }
-
-        public Result<string> SendMultipleMedia(List<IBotMediaFile> mediaFiles, string text, SenderInfo sender)
-        {
-            Result<string> result;
-            if (mediaFiles.First() is IBotOnlineFile)
-            {
-                result = SendText(text, sender);
-                if (result.IsFailed)
-                {
-                    return result;
-                }
-                result = SendText(mediaFiles.First().Path, sender);
-            }
-            else
-            {
-                result = SendMedia(mediaFiles.First(), text, sender);
-            }
-            foreach (var media in mediaFiles.Skip(1))
-            {
-                if (result.IsFailed) return result;
-                if (media is IBotOnlineFile)
-                {
-                    result = SendText(media.Path, sender);
-                }
-                else
-                {
-                    result = SendMedia(media, string.Empty, sender);
-                }
-            }
-
-            return result;
-        }
-
-        public Result<string> SendOnlineMedia(IBotOnlineFile file, string text, SenderInfo sender)
-        {
-            var result = SendText(text, sender);
-            if (result.IsFailed) return result;
-            result = SendText(file.Path, sender);
-            return result;
-        }
-
+        
         public void Restart()
         {
             lock (_lock)
@@ -147,37 +62,11 @@ namespace Kysect.BotFramework.ApiProviders.Discord
             if (message is null) return Task.CompletedTask;
             var context = new SocketCommandContext(_client, message);
             if (context.User.IsBot || context.Guild is null) return Task.CompletedTask;
-            LoggerHolder.Instance.Debug($"New message event: {context.Message.ToString()}");
-            //TODO: Refactor
-            IBotMessage botMessage = default;
             
-            if (context.Message.Attachments.Count == 0)
-            {
-                botMessage = new BotTextMessage(context.Message.Content);
-            }
-            else if (context.Message.Attachments.Count == 1)
-            {
-                IBotOnlineFile onlineFile =
-                    getOnlineFile(message.Attachments.First().Filename, message.Attachments.First().Url);
-                botMessage = onlineFile is not null
-                    ? new BotSingleMediaMessage(context.Message.Content, onlineFile)
-                    : new BotTextMessage(context.Message.Content);
-            } else 
-            {
-                List<IBotMediaFile> mediaFiles = context.Message.Attachments.Select(attachment => getOnlineFile(attachment.Filename, attachment.Url)).Where(onlineFile => onlineFile is not null).Cast<IBotMediaFile>().ToList();
+            LoggerHolder.Instance.Debug($"New message event: {context.Message.ToString()}");
 
-                if (!mediaFiles.Any())
-                {
-                    botMessage = new BotTextMessage(context.Message.Content);
-                } else if (mediaFiles.Count == 1)
-                {
-                    botMessage = new BotSingleMediaMessage(context.Message.Content, mediaFiles.First());
-                }
-                else
-                {
-                    botMessage = new BotMultipleMediaMessage(context.Message.Content, mediaFiles);
-                }
-            }
+            IBotMessage botMessage = parseMessage(message, context);
+
             OnMessage?.Invoke(context.Client,
                 new BotEventArgs(
                     botMessage,
@@ -187,7 +76,37 @@ namespace Kysect.BotFramework.ApiProviders.Discord
                         context.User.Username
                     )
                 ));
+            
             return Task.CompletedTask;
+        }
+
+        private IBotMessage parseMessage(SocketUserMessage message, SocketCommandContext context)
+        {
+            if (context.Message.Attachments.Count == 0)
+                return new BotTextMessage(context.Message.Content);
+
+            if (context.Message.Attachments.Count == 1)
+            {
+                var onlineFile = getOnlineFile(message.Attachments.First().Filename, message.Attachments.First().Url);
+                return onlineFile is not null
+                    ? new BotSingleMediaMessage(context.Message.Content, onlineFile)
+                    : new BotTextMessage(context.Message.Content);
+            }
+
+            List<IBotMediaFile> mediaFiles = context.Message.Attachments
+                                                    .Select(attachment => getOnlineFile(attachment.Filename, attachment.Url))
+                                                    .Where(onlineFile => onlineFile is not null).Cast<IBotMediaFile>().ToList();
+
+            if (!mediaFiles.Any())
+                return new BotTextMessage(context.Message.Content);
+             
+            
+            if (mediaFiles.Count == 1)
+                return new BotSingleMediaMessage(context.Message.Content, mediaFiles.First());
+            
+            
+            return new BotMultipleMediaMessage(context.Message.Content, mediaFiles);
+            
         }
 
         private IBotOnlineFile getOnlineFile(string filename, string url)
@@ -210,14 +129,112 @@ namespace Kysect.BotFramework.ApiProviders.Discord
             return MediaTypeEnum.Undefined;
         }
         
-        private void Initialize()
+        public Result<string> SendMultipleMedia(List<IBotMediaFile> mediaFiles, string text, SenderInfo sender)
         {
-            _client = new DiscordSocketClient();
+            Result<string> result;
+            if (mediaFiles.First() is IBotOnlineFile onlineFile)
+            {
+                result = SendOnlineMedia(onlineFile, text, sender);
+            }
+            else
+            {
+                result = SendMedia(mediaFiles.First(), text, sender);
+            }
+            
+            foreach (var media in mediaFiles.Skip(1))
+            {
+                if (result.IsFailed) return result;
+                if (media is IBotOnlineFile)
+                {
+                    result = SendOnlineMedia(media as IBotOnlineFile, String.Empty,  sender);
+                }
+                else
+                {
+                    result = SendMedia(media, string.Empty, sender);
+                }
+            }
 
-            _client.LoginAsync(TokenType.Bot, _settings.AccessToken);
+            return result;
+        }
+        
+        public Result<string> SendMedia(IBotMediaFile mediaFile, string text, SenderInfo sender)
+        {
+            var result = checkText(text);
+            if (result.IsFailed)
+                return result;
+            
+            var task = _client.GetGuild((ulong) sender.GroupId)
+                .GetTextChannel((ulong) sender.UserSenderId)
+                .SendFileAsync(mediaFile.Path, text);
+            try
+            {
+                task.Wait();
+                return Result.Ok("Message send");
+            }
+            catch (Exception e)
+            {
+                const string message = "Error while sending message";
+                LoggerHolder.Instance.Error(e, message);
+                return Result.Fail(new Error(message).CausedBy(e));
+            }
+        }
+        
+        public Result<string> SendOnlineMedia(IBotOnlineFile file, string text, SenderInfo sender)
+        {
+            if (text.Length != 0)
+            {
+                var result = sendText(text, sender);
+                if (result.IsFailed) return result;
+            }
+            
+            return sendText(file.Path, sender);
+        }
+        public Result<string> SendTextMessage(string text, SenderInfo sender)
+        {
+            if (text.Length == 0)
+            {
+                LoggerHolder.Instance.Error($"The message wasn't sent by the command " +
+                                            $"\"{PingCommand.Descriptor.CommandName}\", the length must not be zero.");
+                return Result.Ok();
+            }
+            
+            return sendText(text, sender);
+        }
+        
+        private Result<string> sendText(string text, SenderInfo sender)
+        {
+            Result<string> result = checkText(text);
+            if (result.IsFailed)
+                return result;
+            
+            var task = _client.GetGuild((ulong) sender.GroupId)
+                .GetTextChannel((ulong) sender.UserSenderId)
+                .SendMessageAsync(text);
 
-            _client.MessageReceived += ClientOnMessage;
-            _client.StartAsync();
+            try
+            {
+                task.Wait();
+                return Result.Ok("Message send");
+            }
+            catch (Exception e)
+            {
+                string message = "Error while sending message";
+                LoggerHolder.Instance.Error(e, message);
+                return Result.Fail(new Error(message).CausedBy(e));
+            }
+        }
+
+        private Result<string> checkText(string text)
+        {
+            if (text.Length > 2000)
+            {
+                string subString = text.Substring(0, 99) + "...";
+                string errorMessage = $"The message wasn't sent by the command " +
+                                      $"\"{PingCommand.Descriptor.CommandName}\", the length is too big.";
+                return Result.Fail(new Error(errorMessage).CausedBy(subString));
+            }
+
+            return Result.Ok();
         }
     }
 }
